09a99615bf0f676233d01d35b4220ca3a5f604cc
Your Name <you@example.com>
2022-11-09 17:44:21 -0500

    Go check version or install and add to PATH

---
+++ Go/CheckInstallGo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+if command_exists go ; then
+    echo "Golang is already installed"
+    go version
+else
+  echo "Install dependencies"
+  sudo apt update
+  sudo apt install build-essential jq -y
+
+  wget https://studygolang.com/dl/golang/go1.19.3.linux-amd64.tar.gz
+  tar -xvf go1.19.3.linux-amd64.tar.gz
+  sudo mv go /usr/local
+
+  echo "" >> ~/.profile
+  echo 'export GOPATH=$HOME/go' >> ~/.profile
+  echo 'export GOROOT=/usr/local/go' >> ~/.profile
+  echo 'export GOBIN=$GOPATH/bin' >> ~/.profile
+  echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.profile
+
+  #source ~/.profile
+  . ~/.profile
+  go version
+fi
+

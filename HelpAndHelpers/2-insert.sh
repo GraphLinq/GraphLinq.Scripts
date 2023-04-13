4461e7fbe047902a3f2983f64f49de1e55859b3d
jrbgit <joanas@gmail.com>
2023-04-13 01:49:16 +0000

    updated node_schema parser

--- HelpAndHelpers/2-insert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#wget https://raw.githubusercontent.com/GraphLinq/GraphLinq.IDE/master/js/node_schema.json
-
-python3 insert.py
-
-#rm -f node_schema.json
-
--- HelpAndHelpers/insert.py
+++ HelpAndHelpers/insert.py
@@ -1,21 +1,44 @@
 import json
-#import pymysql
+import pymysql
 import sys
-from decouple import config
+#from decouple import config
+from prettytable import PrettyTable
 
-HOST = config('HELPER_HOST')
-USER = config('HELPER_USER')
-PASS = config('HELPER_PASS')
-DB = config('HELPER_DB')
+# PrettyTable
+x = PrettyTable()
+x.field_names = ["Total", "Already Exists", "Record Created"]
 
+# Counters
+COUNT = 0
+UPDATED = 0
+SAME = 0
+
+# DB Config
+#HOST = config('HELPER_HOST')
+#USER = config('HELPER_USER')
+#PASS = config('HELPER_PASS')
+#DB = config('HELPER_DB')
+
+# DB Manual Config
+HOST = ""
+USER = ""
+PASS = ""
+DB = ""
+
+# DB Connect
 conn = pymysql.connect(host=HOST,
-                        database=DB,
-                        user=USER,
-                        password=PASS)
+    database=DB,
+    user=USER,
+    password=PASS)
 cur  = conn.cursor()
 
+# Generic No Additional Help Available Message
 h = "<hr><p><b>No additional help available for this block.</b></p>"
 
+# Direct Link to Help Document
+l = "https://https://docs.graphlinq.io"
+
+# Begin loop
 with open('node_schema.json',) as data_file:
     data = json.load(data_file)
     for v in data:
@@ -25,9 +48,23 @@ with open('node_schema.json',) as data_file:
         n = v['NodeBlockType'].strip()
         d = v['NodeDescription'].strip()
         print(t)
+
         query = f"INSERT IGNORE INTO helper (nodetype, nodehelp, nodelink) VALUES ('{t}', '{h}', '{l}')"
         cur.execute(query)
         print(f"{cur.rowcount} details inserted")
+
+        if (cur.rowcount == 0):
+            SAME += 1
+        else:
+            UPDATED =+ cur.rowcount
+
+        COUNT += 1
+        print()
+
 conn.commit()
 conn.close()
-
+print("COMPLETE")
+print()
+x.add_row([COUNT, SAME, UPDATED])
+print(x)
+print()
